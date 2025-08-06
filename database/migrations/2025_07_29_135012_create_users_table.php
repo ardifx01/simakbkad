710eb34bc695c41e9dc8e26042dffd8e6d88160d
RivajaBonari <vajautama01@gmail.com>
2025-08-06 10:05:06 +0700

    perbaikan env

--- database/migrations/2025_07_29_135012_create_users_table.php
+++ database/migrations/2025_07_29_135012_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('nama');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->foreignId('role_id')->constrained();
             $table->foreignId('bidang_id')->nullable()->constrained();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +17,24 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        DB::table('roles')->insert([
+            'id' => 1,
+            'nama_role' => 'Admin',
+            'role_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
+        DB::table('users')->insert([
+            'nama' => 'Test User',
             'email' => 'test@example.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+            'remember_token' => Str::random(10),
+            'role_id' => 1,
+            'bidang_id' => null,
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
     }
 }
